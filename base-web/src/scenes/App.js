2a7bc05e62a6621a43fdcc68f91447f59124f407
erick <erickkay272@gmail.com>
2020-03-15 18:59:12 -0300

    Starting Project Base GreenCode

---
+++ base-web/src/scenes/App.js
@@ -0,0 +1,11 @@
+import React from '../../node_modules/react';
+import IntroView from './onboarding/intro/intro'
+function App() {
+  return (
+    <>
+      <IntroView />
+    </>
+  );
+}
+
+export default App;
---
+++ base-web/src/scenes/onboarding/intro/intro.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class IntroView extends Component {
+
+    render() {
+        return (
+            <>
+                <button class="btn waves-effect waves-light" type="submit" name="action">Submit
+    <i class="material-icons right">send</i>
+  </button>
+            </>
+
+        );
+    }
+}
+
+export default IntroView;
