ded23437a6dbf30ea7d2c93b713e1894d4250b5d
erick <erickkay272@gmail.com>
2020-03-22 23:30:44 -0300

    Aplicando estilização na primeira parte

--- base-web/src/scenes/onboarding/intro/intro.jsx
+++ base-web/src/scenes/onboarding/intro/intro.jsx
@@ -5,11 +5,11 @@ class IntroView extends Component {
     render() {
         return (
             <>
-                <div className="row">
+                <div className="row backgroundImageHome">
                     <nav>
                         <div class="nav-wrapper backgroundDefault">
-                            <a href="#" class="brand-logo">Logo</a>
-                            <ul id="nav-mobile" class="right hide-on-med-and-down">
+                            <a href="#" class="brand-logo"></a>
+                            <ul id="nav-mobile" class="right hide-on-med-and-down backgroundDefault">
                                 <li><a href="">Mundo Limpo</a></li>
                                 <li><a href="">O Que Queremos</a></li>
                                 <li><a href="">Nossa Missão</a></li>
@@ -18,7 +18,7 @@ class IntroView extends Component {
                         </div>
                     </nav>
                     <div className="col xl12 imageBackground">
-                        <div className="container backgroundDefault">
+                        <div className="container">
                             <div className="row">
                                 <center>
                                     <h1>TESTE</h1>
@@ -35,15 +35,33 @@ class IntroView extends Component {
                     </div>
                 </div>
                 <section className="row backgroundDefault1">
-                    <div className="col xl6">
-                        <h2>LADO ESQUERDO</h2>
-                    </div>
-                    <div className="col xl6">
-                        <h2>LADO DIREITO</h2>
-                    </div>
+                    <article className="col xl6 whoIsGreenCode">
+                        <h1>Quem é a Green Code</h1>
+                        <p>
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                            when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                        </p>
+                    </article>
+                    <article className="col xl6 whatOurGoal">
+                        <h1>Qual Nosso Objetivo</h1>
+                        <p>
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                            when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                        </p>
+                    </article>
                     <footer>
                         <center><button>BOTÃO RODAPÉ</button></center>
                     </footer>
+                </section>
+                <section className="row color3">
+                    <article className="col xl6 color1">
+                        <h4>Esquerda</h4>
+                    </article>
+                    <article className="col xl6 color2">
+                        <h4>Direita</h4>
+                    </article>
                 </section>
             </>
         );
